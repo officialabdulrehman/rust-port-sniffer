a2e7156f2a8b05c5788589042bd04332d0295283
NizTheDev <arehman08@hotmail.com>
2022-11-16 21:11:12 +0500

    feat: made the basic version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use std::{
+    env,
+    io::{self, Write},
+    net::{IpAddr, TcpStream},
+    str::FromStr,
+    sync::mpsc::{channel, Sender},
+    thread,
+};
+
+const END_PORT: u16 = 65535;
+
+struct Input {
+    flag: String,
+    ip: IpAddr,
+    threads: u16,
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, ip: IpAddr, threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((ip, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+        if (END_PORT - port) <= threads {
+            break;
+        }
+        port += threads
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let rawArgs: Vec<String> = env::args().collect();
+    let args = Input {
+        flag: String::from("-i"),
+        ip: IpAddr::from_str("192.168.10.1").unwrap(),
+        threads: 4,
+    };
+    let (tx, rx) = channel();
+
+    for i in 0..args.threads {
+        let tx = tx.clone();
+        thread::spawn(move || scan(tx, i, args.ip, args.threads));
+    }
+
+    let mut result = vec![];
+
+    drop(tx);
+
+    for p in rx {
+        result.push(p);
+    }
+
+    println!("");
+
+    result.sort();
+
+    for port in result {
+        println!("{} is open", port);
+    }
 }
